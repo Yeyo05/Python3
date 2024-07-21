c939c66f538857525854bf82dd02ea69714b4001
Yeyo05 <dfpj.05@gmail.com>
2024-07-21 09:44:09 -0600

    Se agrega codigo

---
+++ Estructuras.py
@@ -0,0 +1,13 @@
+edad = 61
+if edad >0 and edad<6:
+    print("Esta en primera estancia")
+elif edad>=6 and edad<12:
+    print("Esta en la infancia")
+elif edad>=12 and edad<19:
+    print("Esta en la adolescencia")
+elif edad>=19 and edad<28:
+    print("Esta en la juventud")
+elif edad>=28 and edad<60:
+    print("Esta en la adultez")
+elif edad>60:
+    print("Es adulto mayor")
